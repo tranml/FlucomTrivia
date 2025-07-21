192d17134cd85f806d0c3bd8e8e10387094801ba
Luan Tran <developedbyluan@gmail.com>
2025-07-21 10:17:14 +0700

    design Instructions text, layout and style

---
+++ FlucomTrivia/Views/Instructions.swift
@@ -0,0 +1,55 @@
+//
+//  Instructions.swift
+//  FlucomTrivia
+//
+//  Created by Tran Luan on 21/7/25.
+//
+
+import SwiftUI
+
+struct Instructions: View {
+    var body: some View {
+        ZStack {
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            
+            VStack {
+                Image(.appiconwithradius)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150)
+                
+                ScrollView {
+                    Text("How To Play")
+                        .font(.largeTitle)
+                        .padding()
+                    
+                    VStack(alignment: .leading, spacing: 20) {
+                        Text("Welcome to HP Trivia! In this game you will be asked random questions from the HP books and you must guess the right answer or you will lose points!😱")
+                        
+                        Text("Each question is worth 5 points, but if you guess a wrong answer, you lose 1 point.")
+                        
+                        Text("If you are struggling with a question, there is an option to reveal a hint or reveal the book that answers the question. But beware! Using these also removes 1 point each.")
+                        
+                        Text("When you select the correct answer, you will be awarded all the points left for that question and they will be added to your total score.")
+                    }
+                    .font(.title3)
+                    .padding(.horizontal)
+                    
+                    Text("Good Luck!")
+                        .font(.title)
+                    
+                    
+                }
+                .border(.green, width: 2)
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    Instructions()
+}
