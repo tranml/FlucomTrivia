1db68b2fc92ad834d03b0011e5ffa6453f42faaf
Luan Tran <developedbyluan@gmail.com>
2025-07-21 11:29:25 +0700

    ContentView: add game logo and title

--- FlucomTrivia/Views/ContentView.swift
+++ FlucomTrivia/Views/ContentView.swift
@@ -14,15 +14,28 @@ struct ContentView: View {
                 Image(.hogwarts)
                     .resizable()
                     .frame(width: geo.size.width * 3, height: geo.size.height)
+                    .padding(.top, 3)
+                    .phaseAnimator([false, true]) { content, phase in
+                        content
+                            .offset(x: phase ? geo.size.width/1.1 : -geo.size.width/1.1)
+                    } animation: { _ in
+                            .linear(duration: 60)
+                    }
+                
+                VStack {
+                    Image(systemName: "bolt.fill")
+                        .imageScale(.large)
+                        .font(.largeTitle)
+                    
+                    Text("Flucom")
+                        .font(.custom("PartyLetPlain", size: 70))
+                        .padding(.bottom, -50)
+                    
+                    Text("Trivia")
+                        .font(.custom("PartyLetPlain", size: 60))
+                }
             }
             .frame(width: geo.size.width, height: geo.size.height)
-            .padding(.top, 3)
-            .phaseAnimator([false, true]) { content, phase in
-                content
-                    .offset(x: phase ? geo.size.width/1.1 : -geo.size.width/1.1)
-            } animation: { _ in
-                    .linear(duration: 60)
-            }
         }
         .ignoresSafeArea()
     }
