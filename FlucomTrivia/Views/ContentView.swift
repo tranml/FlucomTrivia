42603dd9a3b78af34360f6999c35dcd49356b799
Luan Tran <developedbyluan@gmail.com>
2025-07-21 11:21:08 +0700

    ContentView: Image: .phaseAnimator: linear

--- FlucomTrivia/Views/ContentView.swift
+++ FlucomTrivia/Views/ContentView.swift
@@ -16,6 +16,13 @@ struct ContentView: View {
                     .frame(width: geo.size.width * 3, height: geo.size.height)
             }
             .frame(width: geo.size.width, height: geo.size.height)
+            .padding(.top, 3)
+            .phaseAnimator([false, true]) { content, phase in
+                content
+                    .offset(x: phase ? geo.size.width/1.1 : -geo.size.width/1.1)
+            } animation: { _ in
+                    .linear(duration: 60)
+            }
         }
         .ignoresSafeArea()
     }
