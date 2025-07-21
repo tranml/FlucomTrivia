19fc4a0efc61ef398ecdaba2106b01fb67999e1a
Luan Tran <developedbyluan@gmail.com>
2025-07-21 18:15:03 +0700

    add audioPlayer to play mp3

--- FlucomTrivia/Views/ContentView.swift
+++ FlucomTrivia/Views/ContentView.swift
@@ -6,8 +6,10 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
     @State private var animateViewsIn = false
     
     var body: some View {
@@ -52,7 +54,15 @@ struct ContentView: View {
         .ignoresSafeArea()
         .onAppear {
             animateViewsIn = true
+            playAudio()
         }
+    }
+    
+    func playAudio() {
+        let sound = Bundle.main.path(forResource: "magic-in-the-air", ofType: "mp3")
+        audioPlayer = try! AVAudioPlayer(contentsOf: URL(filePath: sound!))
+        audioPlayer.numberOfLoops = -1
+        audioPlayer.play()
     }
 }
 
