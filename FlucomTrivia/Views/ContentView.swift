7ed81f16adc2534bd8c1a49b6589b453705a043f
Luan Tran <developedbyluan@gmail.com>
2025-07-21 11:07:32 +0700

    ContentView: modify default behavior of GeometryReader: top-left focus to center

--- FlucomTrivia/Views/ContentView.swift
+++ FlucomTrivia/Views/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         GeometryReader { geo in
-            
+            ZStack {
+                
+            }
+            .frame(width: geo.size.width, height: geo.size.height)
         }
 //        .ignoresSafeArea()
     }
