eb42a654d6ffaf0ddf8e4c6880bf4dff752bbc3f
Luan Tran <developedbyluan@gmail.com>
2025-07-21 09:25:01 +0700

    Initial Commit

---
+++ FlucomTrivia/FlucomTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlucomTriviaApp.swift
+//  FlucomTrivia
+//
+//  Created by Tran Luan on 21/7/25.
+//
+
+import SwiftUI
+
+@main
+struct FlucomTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
