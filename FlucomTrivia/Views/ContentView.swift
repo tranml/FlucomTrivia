7483eb2bf466fdb5b0ca8071553219ebaf9e8460
Luan Tran <developedbyluan@gmail.com>
2025-07-21 18:44:25 +0700

    add transition animation to the play button

--- FlucomTrivia/Views/ContentView.swift
+++ FlucomTrivia/Views/ContentView.swift
@@ -50,25 +50,30 @@ struct ContentView: View {
                     Spacer()
                     Spacer()
                     Spacer()
-
-                    Button {
-                        
-                    } label: {
-                        Text("Play")
-                            .font(.largeTitle)
-                            .foregroundStyle(.white)
-                            .padding(.vertical, 7)
-                            .padding(.horizontal, 50)
-                            .background(.brown)
-                            .clipShape(.rect(cornerRadius: 7))
-                            .shadow(radius: 5)
-                            .scaleEffect(scalePlayButton ? 1.2 : 1)
-                            .onAppear {
-                                withAnimation(.easeInOut(duration: 1.3).repeatForever()) {
-                                    scalePlayButton.toggle()
-                                }
+                    VStack {
+                        if animateViewsIn {
+                            Button {
+                                // Play a game
+                            } label: {
+                                Text("Play")
+                                    .font(.largeTitle)
+                                    .foregroundStyle(.white)
+                                    .padding(.vertical, 7)
+                                    .padding(.horizontal, 50)
+                                    .background(.brown)
+                                    .clipShape(.rect(cornerRadius: 7))
+                                    .shadow(radius: 5)
+                                    .scaleEffect(scalePlayButton ? 1.2 : 1)
+                                    .onAppear {
+                                        withAnimation(.easeInOut(duration: 1.3).repeatForever()) {
+                                            scalePlayButton.toggle()
+                                        }
+                                    }
                             }
+                            .transition(.offset(y: geo.size.height/3))
+                        }
                     }
+                    .animation(.easeOut(duration: 0.7).delay(2), value: animateViewsIn                      )
                     
                     Spacer()
                 }
